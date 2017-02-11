bf954d65c3a1b77b2e8baba635302e1082c8504c
KaiP-598 <kaipengwu598@live.cn>
2017-02-11 18:09:32 +1100

    customclass

---
+++ Lingo_01/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Lingo_01
+//
+//  Created by WuKaipeng on 11/02/2017.
+//  Copyright © 2017 WuKaipeng. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY : CGFloat = 120 / 255.0
---
+++ Lingo_01/FancyView.swift
@@ -0,0 +1,23 @@
+//
+//  FancyView.swift
+//  Lingo_01
+//
+//  Created by WuKaipeng on 11/02/2017.
+//  Copyright © 2017 WuKaipeng. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+class FancyView: UIView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1, height: 1)
+    }
+    
+    
+}
